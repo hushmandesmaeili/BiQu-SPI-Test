1f0be3e399ee356386022a9d4349e98db998bd34
echandler5956f <ethanchandler5956f@gmail.com>
2023-03-17 20:29:17 -0400

    work

--- motorApp/main.cpp
+++ motorApp/main.cpp
@@ -52,9 +52,20 @@ int main() {
   // Setup  and Open SPI
   init_spi_biqu();
 
-  spi_command_t *command = new spi_command_t();
-  spi_data_t *data = new spi_data_t();
-  spi_biqu_send_receive(command, data);
+  // spi_command_t *command = new spi_command_t();
+  spi_data_t data;
+  spi_command_t command;
+  command.q_des_abad[0] = 1;
+  command.q_des_abad[1] = 2;
+  command.q_des_abad[2] = 3;
+  command.q_des_abad[3] = 4;
+
+  data.q_abad[0] = 0;
+  data.q_abad[1] = 0;
+  data.q_abad[2] = 0;
+  data.q_abad[3] = 0;
+
+  spi_biqu_send_receive(&command, &data);
 
   // uint16_t tx_buf[4];
   // uint16_t rx_buf[6];
