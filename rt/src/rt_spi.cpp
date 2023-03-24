fc0be1c2d1bd193eff14c7c35741cde19d7a0b72
janieleung <janie11vlz@gmail.com>
2023-03-24 19:33:01 -0400

    sending full struct

--- rt/src/rt_spi.cpp
+++ rt/src/rt_spi.cpp
@@ -63,7 +63,6 @@ const float abad_offset[4] = {0, 0, 0, 0};
 const float hip_offset[4] = {0, 0, 0, 0};
 const float knee_offset[4] = {0, 0, 0, 0};
 
-
 /*!
  * Compute SPI message checksum
  * @param data : input
@@ -399,33 +398,33 @@ void spi_to_spine_biqu(spi_command_t *cmd, spine_biqu_cmd_t *spine_cmd)
     spine_cmd->q_des_hip[i] =
         (cmd->q_des_hip[i] * hip_side_sign[i]) +
         hip_offset[i];
-      spine_cmd->q_des_knee[i] =
-          (cmd->q_des_knee[i] / knee_side_sign[i]) +
-          knee_offset[i];
+    spine_cmd->q_des_knee[i] =
+        (cmd->q_des_knee[i] / knee_side_sign[i]) +
+        knee_offset[i];
 
-      spine_cmd->qd_des_abad[i] =
-          cmd->qd_des_abad[i] * abad_side_sign[i];
-      spine_cmd->qd_des_hip[i] =
-          cmd->qd_des_hip[i] * hip_side_sign[i];
-      spine_cmd->qd_des_knee[i] =
-          cmd->qd_des_knee[i] / knee_side_sign[i];
+    spine_cmd->qd_des_abad[i] =
+        cmd->qd_des_abad[i] * abad_side_sign[i];
+    spine_cmd->qd_des_hip[i] =
+        cmd->qd_des_hip[i] * hip_side_sign[i];
+    spine_cmd->qd_des_knee[i] =
+        cmd->qd_des_knee[i] / knee_side_sign[i];
 
-      spine_cmd->kp_abad[i] = cmd->kp_abad[i];
-      spine_cmd->kp_hip[i] = cmd->kp_hip[i];
-      spine_cmd->kp_knee[i] = cmd->kp_knee[i];
+    spine_cmd->kp_abad[i] = cmd->kp_abad[i];
+    spine_cmd->kp_hip[i] = cmd->kp_hip[i];
+    spine_cmd->kp_knee[i] = cmd->kp_knee[i];
 
-      spine_cmd->kd_abad[i] = cmd->kd_abad[i];
-      spine_cmd->kd_hip[i] = cmd->kd_hip[i];
-      spine_cmd->kd_knee[i] = cmd->kd_knee[i];
+    spine_cmd->kd_abad[i] = cmd->kd_abad[i];
+    spine_cmd->kd_hip[i] = cmd->kd_hip[i];
+    spine_cmd->kd_knee[i] = cmd->kd_knee[i];
 
-      spine_cmd->tau_abad_ff[i] =
-          cmd->tau_abad_ff[i] * abad_side_sign[i];
-      spine_cmd->tau_hip_ff[i] =
-          cmd->tau_hip_ff[i] * hip_side_sign[i];
-      spine_cmd->tau_knee_ff[i] =
-          cmd->tau_knee_ff[i] * knee_side_sign[i];
+    spine_cmd->tau_abad_ff[i] =
+        cmd->tau_abad_ff[i] * abad_side_sign[i];
+    spine_cmd->tau_hip_ff[i] =
+        cmd->tau_hip_ff[i] * hip_side_sign[i];
+    spine_cmd->tau_knee_ff[i] =
+        cmd->tau_knee_ff[i] * knee_side_sign[i];
 
-      spine_cmd->flags[i] = cmd->flags[i];
+    spine_cmd->flags[i] = cmd->flags[i];
   }
   // spine_cmd->checksum = xor_checksum((uint32_t *)spine_cmd, 64);
 }
