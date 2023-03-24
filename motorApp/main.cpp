4c9af6347d31fb7baca412e4055f716a24b4b9d7
janieleung <janie11vlz@gmail.com>
2023-03-24 19:36:42 -0400

    sending full struct

--- motorApp/main.cpp
+++ motorApp/main.cpp
@@ -131,10 +131,10 @@ int main()
   command.tau_knee_ff[2] = 59.7;
   command.tau_knee_ff[3] = 60.8;
 
-  command.flags[0] = 61.9;
-  command.flags[1] = 62.0;
-  command.flags[2] = 63.1;
-  command.flags[3] = 64.2;
+  command.flags[0] = 61;
+  command.flags[1] = 62;
+  command.flags[2] = 63;
+  command.flags[3] = 64;
 
   data.q_abad[0] = 0;
   data.q_abad[1] = 0;
