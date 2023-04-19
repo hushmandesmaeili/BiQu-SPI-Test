4ede367beb1600a8c48b06f26da98808243aa457
hushmand.esmaeili <hushmand6@gmail.com>
2023-04-19 07:57:04 -0400

    Test for periodic spi transmit

--- motorApp/main.cpp
+++ motorApp/main.cpp
@@ -176,9 +176,9 @@ void debug_commands_vect(int command_num) {
 }
 
 void runSpi() {
-  // std::cout << "Send: " << _counter << "\n";
-  debug_commands_vect(_counter);
-  // spi_biqu_send_receive(&(commands[_counter]), &data);
+  std::cout << "Send: " << _counter << "\n";
+  // debug_commands_vect(_counter);
+  spi_biqu_send_receive(&(commands[_counter]), &data);
 }
 
 /*!
