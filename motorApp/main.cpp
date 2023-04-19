814f95cb2da73cce00cb08e2af2e81c88eb01c8e
hushmand.esmaeili <hushmand6@gmail.com>
2023-04-19 17:45:25 -0400

    New data.csv

--- motorApp/main.cpp
+++ motorApp/main.cpp
@@ -30,7 +30,7 @@ float _lastPeriodTime = 0;
 // float _maxRuntime = 0;
 
 int _counter = 0;
-int _maxCounter = 5968;     // 4 seconds duration at _period = 0.002
+int _maxCounter = 9482;     // 4 seconds duration at _period = 0.002
 
 // Declare 2D vector to store data from CSV file
 std::vector<std::vector<float>> dataCsv;
@@ -43,7 +43,7 @@ std::vector<spi_command_t> commands;
 void import_csv() {
   std::ifstream infile("data.csv");
   std::string line;
-  int count = 0;
+  // int count = 0;
   while (std::getline(infile, line)) {
     // Create a stringstream from the line and use getline() again to 
     // split the line into individual values separated by commas
@@ -51,17 +51,17 @@ void import_csv() {
     std::string value_str;
     std::vector<float> row;
     while (std::getline(ss, value_str, ',')) {
-      std::cout << count << "\n";
+      // std::cout << count << "\n";
       float value = std::stof(value_str);
       row.push_back(value);
     }
-    ++count;
+    // ++count;
     // Add the row to the 2D vector
     dataCsv.push_back(row);
   }
 
   // Test code for import
-  std::cout << "Data length (rows): " << dataCsv.size() << "\n";
+  // std::cout << "Data length (rows): " << dataCsv.size() << "\n";
   // std::cout << dataCsv[0][1] << "\n";
   // std::cout << dataCsv[1][1] << "\n";
   // std::cout << dataCsv[2][1] << "\n";
