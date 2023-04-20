61c586cbf68ca7c64f7409ee64fcb81feb1f9b2c
hushmand.esmaeili <hushmand6@gmail.com>
2023-04-19 20:26:04 -0400

    flip signs back for hip

--- rt/src/rt_spi.cpp
+++ rt/src/rt_spi.cpp
@@ -55,8 +55,8 @@ const float disabled_torque[3] = {0.f, 0.f, 0.f};
 
 // TODO-biqu: testing numbers
 const float abad_side_sign[4] = {1, 1, 1, 1};
-// const float hip_side_sign[4] = {-1.f, 1.f, -1.f, 1.f};      // Original
-const float hip_side_sign[4] = {1.f, -1.f, 1.f, -1.f};         // Single leg test
+const float hip_side_sign[4] = {-1.f, 1.f, -1.f, 1.f};      // Original
+// const float hip_side_sign[4] = {1.f, -1.f, 1.f, -1.f};         // Single leg test
 const float knee_side_sign[4] = {1, 1, 1, 1};
 
 // TODO-biqu: testing numbers
